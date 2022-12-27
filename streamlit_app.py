b7e13373bb57396998360effea5137fc9c7c6865
Jay <102709775+ryan-jayke@users.noreply.github.com>
2022-12-27 10:29:43 -0500

    troubleshooting Control of Flow

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-import pandas
+#import pandas
+#import requests
 import snowflake.connector
+from urllib.error import URLERROR
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -24,7 +26,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #take the json data and normalize it
@@ -32,6 +33,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output normalized data to table
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
